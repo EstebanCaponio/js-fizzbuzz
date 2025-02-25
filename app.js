8a61c71f8cfce33484dec68bfeb7b9c08505537f
EstebanCaponio <estebancaponio@gmail.com>
2025-02-25 15:37:44 +0100

    feat: created for loop

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Ciao Esteban");
+// creare un ciclo for che si ripete finchè il valore di "i" non supera 100
+for (let i = 0; i >= 100; i++);
+// SE "i" è divisibile per 3 AND 5
+// stampa FizzBuzz
+// altrimenti SE è divisibile per 3
+// stampa Fizz
+// altrimenti SE è divisibile per 5
+// stampa Buzz
+// altrimenti stampa "i"
