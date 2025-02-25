43be1414c611801826ce6e2ef10238416bcea72a
EstebanCaponio <estebancaponio@gmail.com>
2025-02-25 15:58:16 +0100

    feat: created 2nd condition

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Ciao Esteban");
+console.log("Esercizio FizzBuzz");
 // creare un ciclo for che si ripete finchè il valore di "i" non supera 100
 for (let i = 1; i <= 100; i++) {
     // SE "i" è divisibile per 3 AND 5
@@ -6,10 +6,10 @@ for (let i = 1; i <= 100; i++) {
         // stampa FizzBuzz
         let i = "FizzBuzz";
         console.log(i);
-    }
-    // altrimenti SE è divisibile per 3
-    // stampa Fizz
-    // altrimenti SE è divisibile per 5
-    // stampa Buzz
-    // altrimenti stampa "i"
-}
+        // altrimenti SE è divisibile per 3
+    } else if (i % 3 == 0) {
+        // stampa Fizz
+        let i = "Fizz";
+        console.log(i);
+        // altrimenti SE è divisibile per 5
+    } 
