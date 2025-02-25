ef12f9f644f1ce3fb29afe9e89726965ffec9bbe
EstebanCaponio <estebancaponio@gmail.com>
2025-02-25 16:16:12 +0100

    feat: added variables

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 console.log("Esercizio FizzBuzz");
 // creare un ciclo for che si ripete finchè il valore di "i" non supera 100
 for (let i = 1; i <= 100; i++) {
+    // creare due variabili per non ripetere i%3==0 e i%5==0
+    let rem3 = i % 3 == 0;
+    let rem5 = i % 5 == 0;
+
     // SE "i" è divisibile per 3 AND 5
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    if (rem3 && rem5) {
         // stampa FizzBuzz
         let i = "FizzBuzz";
         console.log(i);
         // altrimenti SE è divisibile per 3
-    } else if (i % 3 == 0) {
+    } else if (rem3) {
         // stampa Fizz
         let i = "Fizz";
         console.log(i);
         // altrimenti SE è divisibile per 5
-    } else if (i % 5 == 0) {
+    } else if (rem5) {
         // stampa Buzz
         let i = "Buzz";
         console.log(i);
     } else
-    // se non rispetta nessuna condizione stampa "i"
-    console.log(i);
+        // se non rispetta nessuna condizione stampa "i"
+        console.log(i);
 }
